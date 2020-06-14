bcd73596448ed6498921b9b467877e1ca838cc07
Bona Nugroho <bonanugroho>
2020-06-14 23:56:50 +0700

    Using promise

--- solutions.js
+++ solutions.js
@@ -2,6 +2,7 @@
 const request = require("request-promise");
 const cheerio = require('cheerio');
 const fs = require('fs');
+const { resolve } = require("path");
 
 const baseUrl = "https://www.bankmega.com";
 const listUrl = `${baseUrl}/ajax.promolainnya.php?product=0`;
@@ -43,7 +44,8 @@ subcat.forEach(subcat => {
     console.log(subcat.subcatId + "> " + subcat.title);
 });
 
-const extractDetailPromo = async (promoDetailUrl) => {
+const extractDetailPromo = (promoDetailUrl) => {
+
     const detailResult =  request.get(promoDetailUrl)
         // .then(function (fromResolve) {
             var $detailBody = cheerio.load(detailResult);
@@ -69,40 +71,93 @@ const extractDetailPromo = async (promoDetailUrl) => {
 }
 
 const extractPromos = async (promoUrl) => {
-    const result = await request.get(promoUrl);
-    var $body = cheerio.load(result);
-    let promosOnPage = [];
 
-    $body("#promolain li a").each( (index,element) => {
-        const title = $body(element)
-            .children("#imgClass")
-            .attr("title");
-        const detailUrl = `${baseUrl}/` + $body(element)
-            .attr("href");
-        
-        let item =  extractDetailPromo(detailUrl);
-        promosOnPage[index] = item;
+    
 
-        
-        // console.log("title=" + title + " url=" + detailUrl);
-    })
-
-    if (promosOnPage < 1) {
-        // Terminate no result
-        return promosOnPage;
-    } else {
-        const nextPageNumber = parseInt(promoUrl.match(/page=(\d+)$/)[1], 10) + 1;
-        const nextUrl = `${listUrl}&subcat=3&page=${nextPageNumber}`;
-        return promosOnPage.concat( await extractPromos(nextUrl));
+    var options = {
+        uri: promoUrl,
+        transform: function (body) {
+            return cheerio.load(body);
+        }
     }
+
+    request(options)
+        .then( function ($body) {
+            let promosOnPage = [];
+
+            $body("#promolain li a").each( (index,element) => {
+                const title = $body(element)
+                    .children("#imgClass")
+                    .attr("title");
+                const detailUrl = `${baseUrl}/` + $body(element)
+                    .attr("href");
+                
+                // let item =  extractDetailPromo(detailUrl);
+                // promosOnPage[index] = item;
+                
+                promosOnPage.push({title, detailUrl});
+                // console.log("title=" + title + " url=" + detailUrl);
+            })
+
+            // get by page recursively
+            if (promosOnPage < 1) {
+                // Terminate no result
+                return promosOnPage;
+                // return new Promise((resolve) => {
+                //     resolve(promosOnPage); 
+                // });
+            } else {
+                const nextPageNumber = parseInt(promoUrl.match(/page=(\d+)$/)[1], 10) + 1;
+                const nextUrl = `${listUrl}&subcat=3&page=${nextPageNumber}`;
+                return promosOnPage.concat( await extractPromos(nextUrl));
+                // return new Promise((resolve,reject) => {
+                //     extractPromos(nextUrl)
+                //     .then((resolvePromoOnPage) => {
+                //         resolve(promosOnPage.concat( resolvePromoOnPage ));
+                //     })
+                //     .catch((err)=>{
+                //         reject(err);
+                //     })
+                    
+                // });
+            }
+        })
+        .catch( function (err) {
+            console.log(err);
+            // return new Promise((reject) => {
+            //     reject(err);
+            // })
+        })
+
+
+
+
+    // const result = request.get(promoUrl);
+    // var $body = cheerio.load(result);
+    
 }
 
 async function main() {
-    const firstUrl = `${listUrl}&subcat=3&page=1`;
+    const firstUrl = `${listUrl}&subcat=3&page=18`;
 
     let promoResult = await extractPromos(firstUrl);
+    // extractPromos(firstUrl)
 
-    let no = 1;
+    // Promise.all([extractPromos(firstUrl)])
+    // .then(([promoResult]) => {
+    //     console.log("length = " + promoResult.length);
+    // })
+    // .catch(([err]) => {
+    //     console.log(err);
+    // })
+
+    // extractPromos(firstUrl)
+    // .then(promoResult => {
+    //     console.log(promoResult.length);
+    // })
+
+
+    let no = 1;  
     promoResult.forEach(element => {
         console.log(no + "> title=" + element.title + " url=" + element.detailUrl);
         no++;
